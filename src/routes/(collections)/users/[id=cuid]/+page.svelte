ec7db6b0752c3516843e72f7489baa2a1c9ac131
Pukar Sharma <sharmapukar217@gmail.com>
2023-05-07 22:05:48 +0545

    chaged username constrain from required to optional

--- src/routes/(collections)/users/[id=cuid]/+page.svelte
+++ src/routes/(collections)/users/[id=cuid]/+page.svelte
@@ -149,8 +149,7 @@
           bind:value={$form.username}
           aria-invalid={!!$errors.username}
           class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-xl focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-          placeholder="Enter the username"
-          required />
+          placeholder="Enter the username" />
         {#if $errors.username}
           <small class="text-red-500">{$errors.username}</small>
         {/if}
--- src/routes/(collections)/users/create-user/+page.svelte
+++ src/routes/(collections)/users/create-user/+page.svelte
@@ -120,8 +120,7 @@
           bind:value={$form.username}
           aria-invalid={!!$errors.username}
           class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-xl focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-          placeholder="Enter a new username (optional)"
-          required />
+          placeholder="Enter a new username (optional)" />
         {#if $errors.username}
           <small class="text-red-500">{$errors.username}</small>
         {/if}
